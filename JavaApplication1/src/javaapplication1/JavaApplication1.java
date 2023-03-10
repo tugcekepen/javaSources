6158adde6f60f35c8f0ad820f68a096d7df122fa
tugcekepen <kepentugce@gmail.com>
2023-03-11 01:21:47 +0300

    ilk calismalar eklendi

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+
+package javaapplication1;
+
+import java.util.Scanner; //kullanicidan girdi almak için gerekli olan sınıf kütüphanesi
+/**
+ *
+ * @author TugceK
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // System.out.print("Hello world");
+        
+        /*
+        byte b = 0; 
+        int a = b++; 
+        System.out.println(a); // 0
+        System.out.println(b); // 1
+        */
+        
+        /*
+        Scanner girdi = new Scanner(System.in ); //import java.util.Scanner; //Scanner sınıfından nesne üreterek(System.in) kullanıcıdan input alabiliriz.
+        System.out.print("Adınız: ");
+        String name = girdi.next(); //veya nextLine() kullanabilirsin. nextLine() tüm satırı okur.
+        System.out.print("Yaşınız: ");
+        int sayi = girdi.nextInt();
+        System.out.println(name);
+        System.out.println(sayi);
+        */
+        
+        //double türündeki veri virgüllü girilmeli.
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
