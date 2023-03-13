4fbbf518cb01b108898c969f1ecc6117d511072b
tugcekepen <kepentugce@gmail.com>
2023-03-14 00:55:24 +0300

    sayı sıralama ödevi eklendi.

---
+++ JavaApplication1/src/javaapplication1/Siralama.java
@@ -0,0 +1,44 @@
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TugceK
+ */
+
+public class Siralama {
+    public static void main (String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        double num1, num2, num3;
+        
+        System.out.println("Sıralanacak 3 sayıyı sırayla giriniz:");
+        num1 = input.nextDouble();
+        num2 = input.nextDouble();
+        num3 = input.nextDouble();
+        
+        if (num1 < num2 && num1 < num3){
+            if (num2 < num3){
+                System.out.println(num1 + " < " + num2 + " < " + num3);
+            } else {
+                System.out.println(num1 + " < " + num3 + " < " + num2);
+            }
+        } else if (num2 < num3 && num2 < num1) {
+            if (num3 < num1){
+                System.out.println(num2 + " < " + num3 + " < " + num1);
+            } else {
+                System.out.println(num2 + " < " + num1 + " < " + num3);
+            }
+        } else if (num3 < num1 && num3 < num2) {
+            if (num1 < num2){
+                System.out.println(num3 + " < " + num1 + " < " + num2);
+            } else {
+                System.out.println(num3 + " < " + num2 + " < " + num1);
+            }
+        } else {
+            System.out.println("Geçersiz!");
+        }
+    }
+}
