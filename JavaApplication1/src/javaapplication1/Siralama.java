9255aa6e286ffbad963ac918f71222e5d34ba1a3
tugcekepen <kepentugce@gmail.com>
2023-03-14 00:58:51 +0300

    siralama guncellendi.

--- JavaApplication1/src/javaapplication1/Siralama.java
+++ JavaApplication1/src/javaapplication1/Siralama.java
@@ -21,21 +21,21 @@ public class Siralama {
         
         if (num1 < num2 && num1 < num3){
             if (num2 < num3){
-                System.out.println(num1 + " < " + num2 + " < " + num3);
+                System.out.println(num3 + " > " + num2 + " > " + num1);
             } else {
-                System.out.println(num1 + " < " + num3 + " < " + num2);
+                System.out.println(num2 + " > " + num3 + " > " + num1);
             }
         } else if (num2 < num3 && num2 < num1) {
             if (num3 < num1){
-                System.out.println(num2 + " < " + num3 + " < " + num1);
+                System.out.println(num1 + " > " + num3 + " > " + num2);
             } else {
-                System.out.println(num2 + " < " + num1 + " < " + num3);
+                System.out.println(num3 + " > " + num1 + " > " + num2);
             }
         } else if (num3 < num1 && num3 < num2) {
             if (num1 < num2){
-                System.out.println(num3 + " < " + num1 + " < " + num2);
+                System.out.println(num2 + " > " + num1 + " > " + num3);
             } else {
-                System.out.println(num3 + " < " + num2 + " < " + num1);
+                System.out.println(num1 + " > " + num2 + " > " + num3);
             }
         } else {
             System.out.println("Geçersiz!");
